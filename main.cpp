40bc6ed0bea7bb321789537897acdcd65e6345d3
Cole <c-jankk@ufl.edu>
2024-12-04 11:28:44 -0500

    tweaking files

--- main.cpp
+++ main.cpp
@@ -8,7 +8,9 @@
 #include "DataSource.h"
 #include "data_src/City.h"
 #include <cmath>
+
 #include "maxHeap.h"
+#include "SplayTree.h"
 using namespace std;
 using namespace bridges;
 
@@ -47,15 +49,15 @@ double calcScore(const cityData& city, const string& state, int minPop, int maxP
 	return score;
 }
 
-/*
+
 int main(int argc, char **argv) {
 	// create bridges object
-	Bridges bridges (3, "c-jankk", "708194223238");
+	Bridges bridges (3, "c-jankk", "199217990495");
 	bridges.setTitle("City Preference Generator");
 	DataSource ds (&bridges);
 
 	// create preferences object with inputted user preferences (just example preferences until we incorporate the UI)
-	userPreferences preferences(0, 200000, -1000, 100, "FL", 5, 5, 1, 5);
+	userPreferences preferences(100000, 1000000, 100, 400, "IL", 25, 5, 4, 3);
 
 	// set the city params to search for cities inside the min/max populations and elevations
 	unordered_map<string, string> city_params {
@@ -75,25 +77,48 @@ int main(int argc, char **argv) {
 		cityObjects.push_back(cityData(c.getCity(), c.getState(), c.getPopulation(), c.getElevation()));
 	}
 
-	// create heap
-	maxHeap heap;
+    // choose a method to search with by entering 1 or 2, replace with UI elements later
+    int method;
+    cin >> method;
 
-	// for each city object in the cityObjects vector, calculate the city's score and add the city to the heap
-	for (cityData& c : cityObjects) {
-		c.score = calcScore(c, preferences.preferredState, preferences.minPopulation, preferences.maxPopulation, preferences.minAlt, preferences.maxAlt, preferences.popRank, preferences.altRank, preferences.stateRank);
-		heap.insert(c);
-	}
+    if (method == 1) { // do heap method
 
-	// print the cities in order using the pop function
-	cout << "cities ranked by priority: " << endl;
-	int count = preferences.resultCount;
-	while (!heap.empty() && count > 0) {
-		cityData top = heap.pop();
-		cout << top.name << ' ' << top.state << ' ' << top.population << " " << top.score << endl;
-		count--;
-	}
+        // create heap
+        maxHeap heap;
+
+        // for each city object in the cityObjects vector, calculate the city's score and add the city to the heap
+        for (cityData &c: cityObjects) {
+            c.score = calcScore(c, preferences.preferredState, preferences.minPopulation, preferences.maxPopulation,
+                                preferences.minAlt, preferences.maxAlt, preferences.popRank, preferences.altRank,
+                                preferences.stateRank);
+            heap.insert(c);
+        }
+
+        // print the cities in order using the pop function
+        cout << "cities ranked by priority: " << endl;
+        int count = preferences.resultCount;
+        while (!heap.empty() && count > 0) {
+            cityData top = heap.pop();
+            cout << top.name << ' ' << top.state << ' ' << top.population << " " << top.score << endl;
+            count--;
+        }
 
 
-	return 0;
+        return 0;
+    }
+
+    if (method == 2) { // do splay tree method
+        SplayTree tree;
+
+        for (cityData &c: cityObjects) { // insert tree elements
+            c.score = calcScore(c, preferences.preferredState, preferences.minPopulation, preferences.maxPopulation,
+                                preferences.minAlt, preferences.maxAlt, preferences.popRank, preferences.altRank,
+                                preferences.stateRank);
+            tree.root = tree.Insert(tree.root, c);
+        }
+        tree.SetResults(preferences.resultCount);
+        tree.PrintNames(tree.root);
+
+    }
+    return 0;
 }
-*/
